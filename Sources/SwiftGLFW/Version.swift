a52153914739b8974e298dcd3edfa938d9e3e6ef
XeresRazor <shinohara@gmail.com>
2021-02-23 14:09:05 -0500

    Closes #7
    Adds wrappers for version methods.

---
+++ Sources/SwiftGLFW/Version.swift
@@ -0,0 +1,26 @@
+//
+//  Version.swift
+//  
+//
+//  Created by David Green on 2/22/21.
+//
+
+import cglfw
+
+/// Retrieves the major, minor, and revision numbers of the GLFW library.
+/// - Returns: ('major', 'minor', 'revision') version number components.
+/// - Note: The corresponding C function is [glfwGetVersion](https://www.glfw.org/docs/latest/group__init.html#ga9f8ffaacf3c269cc48eafbf8b9b71197).
+public func GLFWGetVersion() -> (major: Int, minor: Int, revision:  Int) {
+    var major: Int32 = 0, minor: Int32 = 0, rev: Int32 = 0
+    glfwGetVersion(&major, &minor, &rev)
+    return (Int(major), Int(minor), Int(rev))
+}
+
+/// Returns the compile-time generated version string of the GLFW library.
+/// - Returns: The GLFW version string,
+/// - Note: The corresponding C function is [glfwGetVersionString](https://www.glfw.org/docs/latest/group__init.html#ga23d47dc013fce2bf58036da66079a657).
+public func GLFWGetVersionString() -> String? {
+    guard let version = glfwGetVersionString() else { return nil }
+    let versionString = String(cString: version)
+    return versionString
+}
